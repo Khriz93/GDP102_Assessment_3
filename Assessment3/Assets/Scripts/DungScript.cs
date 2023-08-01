47b91fcb7587239223955f759307087af20b8151
Khriz93 <hokage93@hotmail.com>
2023-08-02 02:27:32 +1000

    Added the dung holder

---
+++ Assessment3/Assets/Scripts/DungScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungScript : MonoBehaviour
+{
+    public AudioManager audioManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        // if (collision.CompareTag("Ground"))
+        // {
+        //     audioManager.DungSplatSound();
+        // }
+
+        // if (collision.CompareTag("Dung"))
+        // {
+        //     audioManager.DungHitDungSound();
+        // }
+    }
+}
