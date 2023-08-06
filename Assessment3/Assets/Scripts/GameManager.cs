2d8dc2cef8ae75d903055561ad24b4e4d2902960
LateKnightDev <139241390+LateKnightDev@users.noreply.github.com>
2023-08-06 21:00:27 +0700

    Added Prefab Enemy AI

--- Assessment3/Assets/Scripts/GameManager.cs
+++ Assessment3/Assets/Scripts/GameManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public float gameTime = 0;
+    private bool hasEnemy = false;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(GameTimer());
+        StartCoroutine(SpawnEnemy());
     }
 
     // Update is called once per frame
@@ -24,6 +27,16 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(1);
             gameTime++;
             print("Game Time = " + gameTime.ToString());
+        }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (hasEnemy == false)
+        {
+            yield return new WaitForSeconds(5);
+            Instantiate(enemy, new Vector3(0, 5, 0), Quaternion.identity);
+            hasEnemy = true;
         }
     }
 
